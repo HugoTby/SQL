8daaddd7ec6e4a9c68f3145d2243ce2dcc839c8e
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-01-13 16:27:34 +0100

    Create code.sql

---
+++ code.sql
@@ -0,0 +1,7 @@
+SELECT Medecin.nom, Medecin.prenom FROM `Medecin`, Consultation, Patient WHERE Medecin.id = Consultation.idMedecin AND Consultation.idPatient = Patient.id AND Medecin.id = "2"; 
+
+SELECT * FROM `Patient` WHERE nom LIKE 'S%';
+
+SELECT Medicament.nom FROM `Prescription`, Medicament WHERE Medicament.id = Prescription.idMedicament
+
+SELECT Medicament.nom FROM Medicament, Medecin, Prescription, Consultation WHERE Medecin.id = Consultation.idMedecin AND Consultation.id = Prescription.idConsultation AND Prescription.idMedicament=Medicament.id AND Medicament.id = 5; 
