0ff6481aec129a57b6c11c5074aeab5a2ca01b4b
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-01-25 15:28:22 +0100

    Update code.sql

--- code.sql
+++ code.sql
@@ -5,3 +5,5 @@ SELECT * FROM `Patient` WHERE nom LIKE 'S%';
 SELECT Medicament.nom FROM `Prescription`, Medicament WHERE Medicament.id = Prescription.idMedicament
 
 SELECT Medicament.nom FROM Medicament, Medecin, Prescription, Consultation WHERE Medecin.id = Consultation.idMedecin AND Consultation.id = Prescription.idConsultation AND Prescription.idMedicament=Medicament.id AND Medicament.id = 5; 
+
+SELECT Patient.nom , COUNT(Consultation.id) from Patient,Consultation WHERE Patient.id = Consultation.idPatient GROUP BY Patient.nom
